189f713e19b75aa689c2fcb0d6ff38c7ce59cca9
Shubham <25881429+shoebham@users.noreply.github.com>
2024-06-17 09:38:35 +0530

    insert to db in stream

--- app/chat/streaming.py
+++ app/chat/streaming.py
@@ -11,7 +11,7 @@ from app.core.logs import logger
 from app.settings import settings
 
 
-
+from app.db import messages_queries
 async def stream_generator(subscription):
     async with async_timeout.timeout(settings.GENERATION_TIMEOUT_SEC):
         try:
@@ -21,6 +21,7 @@ async def stream_generator(subscription):
                 complete_response = f"{complete_response}{Chunk.get_chunk_delta_content(chunk=chunk)}"
                 yield format_to_event_stream(post_processing(chunk))
             message: Message = Message(model=chunk["model"],message=complete_response,role=ChatRolesEnum.ASSISTANT.value)
+            messages_queries.insert(model=message.model, message=message.message, role=message.role)
             logger.info(f"Complete Streamed Message: {message}")
         except asyncio.TimeoutError:
             raise StreamTimeoutException
