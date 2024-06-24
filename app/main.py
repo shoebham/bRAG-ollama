ee855c295658154ff0734870abbacc018be07fc6
Shubham <25881429+shoebham@users.noreply.github.com>
2024-06-24 20:06:41 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -7,12 +7,21 @@ from app.core.logs import logger
 from app.core.api import router as core_router
 from app.chat.api import router as chat_router
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app  = FastAPI(version=version)
 # Mount the directory containing your HTML file
 app.mount("/Users/shubhamgupta/Desktop/my-stuff/projects/bRAG/static", StaticFiles(directory="static"), name="static")
 
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     
 app.include_router(core_router)
 app.include_router(chat_router)
